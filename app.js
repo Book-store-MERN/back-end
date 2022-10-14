1105ac0974059c56917df6ef4540833ed93a23f2
unknown <scotteychaos@gmail.com>
2022-10-13 21:12:52 -0400

    added mongodb

--- app.js
+++ app.js
@@ -1,1 +1,11 @@
-console.log("Hello World!!")
+const express = require('express');
+const mongoose = require('mongoose')
+
+const app = express();
+
+
+mongoose.connect("mongodb+srv://admin:TKgaGpoQacWrkWhy@cluster0.kms04hf.mongodb.net/?retryWrites=true&w=majority ")
+
+
+
+//TKgaGpoQacWrkWhy
